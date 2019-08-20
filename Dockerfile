apache/spark

[AIRFLOW-5247] Move NPM dependencies up in the Dockerfile (#5870)

Dockerfile
@@ -226,8 +226,6 @@ FROM ${APT_DEPS_IMAGE} as main
 
 SHELL ["/bin/bash", "-o", "pipefail", "-e", "-u", "-x", "-c"]
 
-WORKDIR /opt/airflow
-
 RUN echo "Airflow version: ${AIRFLOW_VERSION}"
 
 ARG AIRFLOW_USER=airflow
@@ -241,6 +239,8 @@ ENV AIRFLOW_HOME=${AIRFLOW_HOME}
 
 ARG AIRFLOW_SOURCES=/opt/airflow
 ENV AIRFLOW_SOURCES=${AIRFLOW_SOURCES}
+
+WORKDIR ${AIRFLOW_SOURCES}
 
 RUN mkdir -pv ${AIRFLOW_HOME} \
     mkdir -pv ${AIRFLOW_HOME}/dags \
@@ -305,6 +305,18 @@ RUN \
         && pip uninstall --yes apache-airflow; \
     fi
 
+# Install NPM dependencies here. The NPM dependencies don't change that often and we already have pip
+# installed dependencies in case of CI optimised build, so it is ok to install NPM deps here
+# Rather than after setup.py is added.
+COPY --chown=airflow:airflow airflow/www/package-lock.json ${AIRFLOW_SOURCES}/airflow/www/package-lock.json
+COPY --chown=airflow:airflow airflow/www/package.json ${AIRFLOW_SOURCES}/airflow/www/package.json
+
+WORKDIR ${AIRFLOW_SOURCES}/airflow/www
+
+RUN gosu ${AIRFLOW_USER} npm ci
+
+WORKDIR ${AIRFLOW_SOURCES}
+
 # Note! We are copying everything with airflow:airflow user:group even if we use root to run the scripts
 # This is fine as root user will be able to use those dirs anyway.
 
@@ -323,27 +335,14 @@ COPY --chown=airflow:airflow airflow/bin/airflow ${AIRFLOW_SOURCES}/airflow/bin/
 # In non-CI optimized build this will install all dependencies before installing sources.
 RUN pip install --no-use-pep517 -e ".[${AIRFLOW_EXTRAS}]"
 
-COPY --chown=airflow:airflow airflow/www/package.json ${AIRFLOW_SOURCES}/airflow/www/package.json
-COPY --chown=airflow:airflow airflow/www/package-lock.json ${AIRFLOW_SOURCES}/airflow/www/package-lock.json
 
 WORKDIR ${AIRFLOW_SOURCES}/airflow/www
 
-ARG BUILD_NPM=true
-ENV BUILD_NPM=${BUILD_NPM}
-
-# Install necessary NPM dependencies (triggered by changes in package-lock.json)
-RUN \
-    if [[ "${BUILD_NPM}" == "true" ]]; then \
-        gosu ${AIRFLOW_USER} npm ci; \
-    fi
-
+# Copy all www files here so that we can run npm building for production
 COPY --chown=airflow:airflow airflow/www/ ${AIRFLOW_SOURCES}/airflow/www/
 
 # Package NPM for production
-RUN \
-    if [[ "${BUILD_NPM}" == "true" ]]; then \
-        gosu ${AIRFLOW_USER} npm run prod; \
-    fi
+RUN gosu ${AIRFLOW_USER} npm run prod
 
 # Cache for this line will be automatically invalidated if any
 # of airflow sources change

potiuk/airflow-docker

(cherry picked from commit a317cd22b199077462c8c8c33203447adc7ef9b6)

Dockerfile
@@ -226,8 +226,6 @@ FROM ${APT_DEPS_IMAGE} as main
 
 SHELL ["/bin/bash", "-o", "pipefail", "-e", "-u", "-x", "-c"]
 
-WORKDIR /opt/airflow
-
 RUN echo "Airflow version: ${AIRFLOW_VERSION}"
 
 ARG AIRFLOW_USER=airflow
@@ -241,6 +239,8 @@ ENV AIRFLOW_HOME=${AIRFLOW_HOME}
 
 ARG AIRFLOW_SOURCES=/opt/airflow
 ENV AIRFLOW_SOURCES=${AIRFLOW_SOURCES}
+
+WORKDIR ${AIRFLOW_SOURCES}
 
 RUN mkdir -pv ${AIRFLOW_HOME} \
     mkdir -pv ${AIRFLOW_HOME}/dags \
@@ -305,6 +305,18 @@ RUN \
         && pip uninstall --yes apache-airflow; \
     fi
 
+# Install NPM dependencies here. The NPM dependencies don't change that often and we already have pip
+# installed dependencies in case of CI optimised build, so it is ok to install NPM deps here
+# Rather than after setup.py is added.
+COPY --chown=airflow:airflow airflow/www_rbac/package-lock.json ${AIRFLOW_SOURCES}/airflow/www_rbac/package-lock.json
+COPY --chown=airflow:airflow airflow/www_rbac/package.json ${AIRFLOW_SOURCES}/airflow/www_rbac/package.json
+
+WORKDIR ${AIRFLOW_SOURCES}/airflow/www_rbac
+
+RUN gosu ${AIRFLOW_USER} npm ci
+
+WORKDIR ${AIRFLOW_SOURCES}
+
 # Note! We are copying everything with airflow:airflow user:group even if we use root to run the scripts
 # This is fine as root user will be able to use those dirs anyway.
 
@@ -323,27 +335,14 @@ COPY --chown=airflow:airflow airflow/bin/airflow ${AIRFLOW_SOURCES}/airflow/bin/
 # In non-CI optimized build this will install all dependencies before installing sources.
 RUN pip install --no-use-pep517 -e ".[${AIRFLOW_EXTRAS}]"
 
-COPY --chown=airflow:airflow airflow/www_rbac/package.json ${AIRFLOW_SOURCES}/airflow/www_rbac/package.json
-COPY --chown=airflow:airflow airflow/www_rbac/package-lock.json ${AIRFLOW_SOURCES}/airflow/www_rbac/package-lock.json
 
 WORKDIR ${AIRFLOW_SOURCES}/airflow/www_rbac
 
-ARG BUILD_NPM=true
-ENV BUILD_NPM=${BUILD_NPM}
-
-# Install necessary NPM dependencies (triggered by changes in package-lock.json)
-RUN \
-    if [[ "${BUILD_NPM}" == "true" ]]; then \
-        gosu ${AIRFLOW_USER} npm ci; \
-    fi
-
+# Copy all www files here so that we can run npm building for production
 COPY --chown=airflow:airflow airflow/www_rbac/ ${AIRFLOW_SOURCES}/airflow/www_rbac/
 
 # Package NPM for production
-RUN \
-    if [[ "${BUILD_NPM}" == "true" ]]; then \
-        gosu ${AIRFLOW_USER} npm run prod; \
-    fi
+RUN gosu ${AIRFLOW_USER} npm run prod
 
 # Cache for this line will be automatically invalidated if any
 # of airflow sources change